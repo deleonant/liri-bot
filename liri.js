7ded56d0a0910dddbd0ab9497cac9ce3c41987c2
Antonio Deleon <Antonio Deleon>
2016-09-03 23:12:41 -0400

    HW week 10

--- liri.js
+++ liri.js
@@ -41,7 +41,7 @@ function twitter(){
 		access_token_secret: twitterKeys.access_token_secret
   });
   // what to search for
-  var params = {screen_name: 'creativelaurels'};
+  var params = {screen_name: 'deleonant'};
   //console.log(params);
 
   // using the npm
